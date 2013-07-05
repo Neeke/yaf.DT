f33be70d5a18b90be0f57c76ac3b4ef4416d6856
Neeke <ciogao@gmail.com>
2013-07-05 21:31:58 +0800

    相册查看

--- app/controllers/Album.php
+++ app/controllers/Album.php
@@ -29,7 +29,7 @@ class AlbumController extends Controller
         $this->model_reply = models_reply::getInstance();
         $this->model       = models_user::getInstance();
 
-        $this->page    = (int)$this->getRequest()->getParam('p', 1);
+        $this->page = (int)$this->getRequest()->getParam('p', 1);
     }
 
     public function indexAction()
@@ -44,11 +44,11 @@ class AlbumController extends Controller
     {
         $this->setMenu('album/mine');
 
-        $my_albums = $this->model_album->myAlbum(1,($this->page - 1) *contast_album::PAGE_SIZE_DEFAULT);
+        $my_albums = $this->model_album->myAlbum(1, ($this->page - 1) * contast_album::PAGE_SIZE_DEFAULT);
 
         $count = $this->model_album->count(array('user_id' => 1));
 
-        $sPage = helper_pages::page2(helper_common::site_url('album/mine'),$count,contast_album::PAGE_SIZE_DEFAULT,$this->page);
+        $sPage = helper_pages::page2(helper_common::site_url('album/mine'), $count, contast_album::PAGE_SIZE_DEFAULT, $this->page);
         $this->set('sPage', $sPage);
         $this->set('myalbums', $my_albums);
     }
@@ -69,20 +69,22 @@ class AlbumController extends Controller
         $album_id   = (int)$this->getRequest()->getParam('a', 0);
         $album_info = $this->model_album->getRow('*', $album_id);
         $items      = $this->model_Items->getAll('*', array('album_id' => (int)$album_id));
+        $userinfo   = $this->model->getRow('user_name,user_id', $album_info['user_id']);
 
-        if (is_array($items) && count($items) > 1){
-            foreach ($items as $v) {
-                $replys[$v['items_id']] = $this->model_reply->getAllByItemId($v['items_id']);
-            }
-        }
+//
+//        if (is_array($items) && count($items) > 1){
+//            foreach ($items as $v) {
+//                $replys[$v['items_id']] = $this->model_reply->getAllByItemId($v['items_id']);
+//            }
+//        }
+//        $this->set('replys', $replys);
 
         $this->db->cache_off();
 
         $this->set('album_info', $album_info);
         $this->set('items', $items);
-        $this->set('replys', $replys);
-        $this->set('userinfo_all', $this->model->getRow('*', $album_info['user_id']));
-        $this->set('other_album_info',$this->model_album->myAlbum($album_info['user_id']));
+
+        $this->set('userinfo', $userinfo);
     }
 
 
