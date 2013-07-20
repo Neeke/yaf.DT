5be299a2ad4cbdb8765e1bc63cf5713e531cf55b
lackhurt <january9527@126.com>
2013-07-20 22:29:46 +0800

    做消息页面

---
+++ app/controllers/Message.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * 
+ * @author pjn
+ *
+ */
+class MessageController extends Controller
+{
+	public function init() {
+		parent::init();
+	}
+
+    public function indexAction() {
+
+    }
+}
