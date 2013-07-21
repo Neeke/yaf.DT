4f008659e333f6a08082f5f30906927687e75b26
Neeke <ciogao@gmail.com>
2013-07-21 21:27:19 +0800

    justlook　data

--- app/controllers/Justlook.php
+++ app/controllers/Justlook.php
@@ -9,32 +9,41 @@ class JustlookController extends Controller
     /**
      * @var models_album
      */
-    private $model_Items;
+    private $model_album;
 
     /**
-     * @var models_album
+     * @var models_albumListen
      */
-    private $model_album;
+    private $model_album_listen;
 
     public function init()
     {
         parent::init();
-        $this->model_album = models_album::getInstance();
-        $this->model       = models_user::getInstance();
-
-        $this->page    = (int)$this->getRequest()->getParam('p', 1);
+        $this->model_album        = models_album::getInstance();
+        $this->model_album_listen = models_albumListen::getInstance();
     }
 
+
+    /**
+     * 设置已经收听的album
+     */
+    private function setListededAlbumIds()
+    {
+        $this->set('listened_album_ids', $this->model_album_listen->myListenedAlbumIds($this->user_id));
+    }
+
+    /**
+     * ＠todo 更好的随机算法　与tag_listened关联
+     */
     public function indexAction()
     {
         $this->db->cache_on();
 
         $this->setMenu('justlook');
+        $this->setListededAlbumIds();
 
-        $my_albums = $this->model_album->myAlbum(1);
+        $my_albums = $this->model_album->justlook();
 
-//        $sPage = helper_pages::page1('/',30,6,$this->page);
-//        $this->set('sPage', $sPage);
         $this->set('myalbums', $my_albums);
     }
 
