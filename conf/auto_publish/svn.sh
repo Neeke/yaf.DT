d19b7e84fe4096bbe9045791705352b72f58b0f8
Neeke <ciogao@gmail.com>
2013-07-29 12:00:37 +0800

    update zh to en

--- conf/auto_publish/svn.sh
+++ conf/auto_publish/svn.sh
@@ -15,12 +15,12 @@ app_conf_path="$cur_path/tmp/trunk/conf"
 
 #the back-file-path
 bak_path="$cur_path/bakdir"
-#上次更新的版本号
+#the last version
 rev_file="$cur_path/rev_file.log"
-#svn记录
+#svn
 svn_log="$cur_path/svn.log"
 
-#获取上次版本号
+#get the last version
 test -f "$rev_file" && latest_rev=`cat $rev_file` || latest_rev=1
 
 #echo $latest_rev
@@ -33,14 +33,14 @@ echo `date +%F......%T` >>$svn_log
 head_rev=`svn info --username=$svn_user --password=$svn_passwd --no-auth-cache --non-interactive --xml $svn_path_1 | grep "revision" | head -1 |cut -d '"' -f2`
 
 if [ "$head_rev" -gt "$latest_rev" ];then
-#=======================================================如果有新版本，则发布
+#=======================================================
 
 	svn export --force --username=$svn_user --password=$svn_passwd --no-auth-cache --non-interactive  $svn_path_1 $tmp_path_1 >>$svn_log
 	#if [ "$?" == 0 ];then
 		echo "$head_rev" > $rev_file
 	#fi
 
-    #避免配置
+    #delete the conf_path
     rm -fr $app_conf_path
 
 
@@ -50,24 +50,24 @@ if [ "$head_rev" -gt "$latest_rev" ];then
         #if [ `ls -al $tmp_path_1|wc -l` != 3 ];then
         cp -pfr $tmp_path_1/trunk/*  $www_path
 
-    #发布到192.168.1.252
+    #scp code to 192.168.1.252
     #	scp -rp $tmp_path_1/*  192.168.1.252:/var/www/WWW_ROOT_DEX
     #	fi
 
-    #发布if [ `ls -al $tmp_path_2|wc -l` != 3 ];then
+    #if [ `ls -al $tmp_path_2|wc -l` != 3 ];then
     #	cp -pfr $tmp_path_2/*  /var/comm_der/
     #	cp -pfr $tmp_path_2/*  /var/comm_dev/
     #	cp -pfr $tmp_path_2/*  /var/comm_dew/
-    #发布到192.168.1.252
+    #192.168.1.252
     #	scp -rp $tmp_path_2/*  192.168.1.252:/var/comm_dex
     #	fi
 
-    #清空临时目录
+    #delete the tmp_path
         rm -fr $tmp_path_1/*
 
-   echo "版本$head_rev发布完成!"
+   echo "the version $head_rev publish finished!"
 
 else
-#=======================================================如果没
-	echo "SVN上没有最新版本!无需发布"
+#=======================================================
+	echo "SVN have no new version!"
 fi
