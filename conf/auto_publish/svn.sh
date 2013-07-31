e76a17d2fd8fbe541be6fb9fd91d328198b625fb
Neeke <ciogao@gmail.com>
2013-07-31 15:18:55 +0800

    update auto_publish_svn script

--- conf/auto_publish/svn.sh
+++ conf/auto_publish/svn.sh
@@ -4,12 +4,15 @@ svn_user="ciogao@gmail.com"
 svn_passwd=""
 
 #www path
-www_path="/data/home/website3/htdocs"
+www_path="/var/www/buyaoji"
 
 #base path
-cur_path="/data/home/website3/ciogao/from_svn"
+cur_path="/var/ciogao/from_svn"
 
 #svn co path
+project_svn="$cur_path/project_svn"
+
+#svn export path
 tmp_path_1="$cur_path/tmp"
 app_conf_path="$cur_path/tmp/trunk/conf"
 
@@ -17,8 +20,10 @@ app_conf_path="$cur_path/tmp/trunk/conf"
 bak_path="$cur_path/bakdir"
 #the last version
 rev_file="$cur_path/rev_file.log"
-#svn
-svn_log="$cur_path/svn.log"
+
+#svn log path
+svn_co_log="$cur_path/svn_co.log"
+svn_export_log="$cur_path/svn_export.log"
 
 #get the last version
 test -f "$rev_file" && latest_rev=`cat $rev_file` || latest_rev=1
@@ -26,16 +31,24 @@ test -f "$rev_file" && latest_rev=`cat $rev_file` || latest_rev=1
 #echo $latest_rev
 
 
-#=======================================================svn start
-echo `date +%F......%T` >>$svn_log
+#=======================================================svn co start
+echo `date +%F......%T` >>$svn_co_log
+
+num=`ls -a $project_svn | wc -l`
+if [ 3 -gt $num ];then
+    svn co $svn_path_1 --username=$svn_user --password=$svn_passwd --no-auth-cache --non-interactive $project_svn >> $svn_co_log
+else
+    svn up $project_svn --username=$svn_user --password=$svn_passwd --no-auth-cache --non-interactive >> $svn_co_log
+fi
 
 #svn check the header-revision
 head_rev=`svn info --username=$svn_user --password=$svn_passwd --no-auth-cache --non-interactive --xml $svn_path_1 | grep "revision" | head -1 |cut -d '"' -f2`
 
 if [ "$head_rev" -gt "$latest_rev" ];then
-#=======================================================
+#=======================================================svn export start
+    echo `date +%F......%T` >>$svn_export_log
 
-	svn export --force --username=$svn_user --password=$svn_passwd --no-auth-cache --non-interactive  $svn_path_1 $tmp_path_1 >>$svn_log
+	svn export --force --username=$svn_user --password=$svn_passwd --no-auth-cache --non-interactive  $project_svn $tmp_path_1 >>$svn_export_log
 	#if [ "$?" == 0 ];then
 		echo "$head_rev" > $rev_file
 	#fi
